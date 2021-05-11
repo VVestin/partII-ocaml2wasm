6de3a4a6759e93eaacc26962303ab580ad3961f4
Weston <weston.metzler@gmail.com>
2021-05-12 00:45:18 +0100

    Update mocha sample tests

--- test/samples.test.js
+++ test/samples.test.js
@@ -1,39 +1,39 @@
-const parse = require('../parser.js')
-const inferTypes = require('../typer.js')
-const comp = require('../comp.js')
+const { compileAndRun } = require('../src/main')
 
 const fs = require('fs')
 const chai = require('chai')
 chai.use(require('chai-as-promised'))
 const expect = chai.expect
 
-describe('Sample programs', function () {
-   const runSample = async sampleName => {
-      const src = String(fs.readFileSync(`test/samples/${sampleName}.ml`))
-      const ast = parse(src)
-      inferTypes(ast, {})
-      const wat = comp(ast)
+const toOCaml = list =>
+   list.length ? `Cons (${list[0]}, ${toOCaml(list.slice(1))})` : 'Nil'
 
-      // Executing
-      const wabt = await require('wabt')()
-      const wasmModule = wabt.parseWat('foo', wat)
-      wasmModule.validate()
-      const instance = (
-         await WebAssembly.instantiate(wasmModule.toBinary({}).buffer)
-      ).instance
-      const wasm = instance.exports
-      return wasm.main()
+describe('Sample programs', function () {
+   const runSample = async (sampleName, input) => {
+      const src = String(
+         fs.readFileSync(`test/samples/${sampleName}.ml`) + ' ' + input
+      )
+
+      return compileAndRun(src)
    }
    ;[
-      ['int-arith', 8],
-      ['float-arith', -4.9375],
-      ['bool-arith', 0],
-      //['8th-power', 65536]
-      //['match', 35]
-      //['int-sqrt', 10]
-      //['fibonacci', 89]
-   ].forEach(([sample, answer]) =>
+      ['int-arith', '', 8],
+      ['float-arith', '', -4.9375],
+      ['bool-arith', '', false],
+      ['8th-power', '', 65536],
+      ['match', '', 35],
+      ['int-sqrt', 100, 10],
+      ['fib-recursive', 10, 89],
+      ['fib-iterative', 10, 89],
+      [
+         'quicksort',
+         `(${toOCaml([3, 1, 6, 2, 4, 5, 0])})`,
+         [0, 1, 2, 3, 4, 5, 6],
+      ],
+      ['age-puzzle', '', [18, 72]],
+      ['8-queens', 10, [6, 3, 1, 8, 4, 9, 7, 5, 2, 0]],
+   ].forEach(([sample, input, answer]) =>
       it(`gets the right anwser for sample ${sample}`, () =>
-         expect(runSample(sample)).to.become(answer))
+         expect(runSample(sample, input)).to.become(answer))
    )
 })
